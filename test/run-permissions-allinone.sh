d7ff765b7423be51d8b844747f45d924db8e8a8c
Martin Nally <mnally@google.com>
2017-03-31 09:33:33 -0700

    fix exports for testing allinone

--- test/run-permissions-allinone.sh
+++ test/run-permissions-allinone.sh
@@ -8,6 +8,9 @@ export INTERNAL_SY_ROUTER_HOST="localhost"
 export INTERNAL_SY_ROUTER_PORT="3200"
 export CACHE_SWEEP_INTERVAL=600000 # (will be divided by SPEEDUP)
 export CACHE_ENTRY_TTL=600000 # (will be divided by SPEEDUP)
+export EDGE_ADDRESS="https://api.e2e.apigee.net"
+export PERMISSIONS_MIGRATION_CLIENTID=${PERMISSIONS_MIGRATION_CLIENTID:-defaultclient} # configure this in your shell when testing
+export PERMISSIONS_MIGRATION_CLIENTSECRET=${PERMISSIONS_MIGRATION_CLIENTSECRET:-defaultsecret} # configure this in your shell when testing
 
 source ../local-export-pg-connection-variables.sh
 #NODE_DEBUG=net
