b7b2417d3bc3f1d22a60c16e2d7c9a2fc8df6ba0
Martin Nally <mnally@google.com>
2017-04-14 08:33:15 -0700

    rename PERMISSISION_MIGRATION_CLIENT to just PERMISSIONS_CLIENT

--- test/run-permissions-allinone.sh
+++ test/run-permissions-allinone.sh
@@ -10,8 +10,8 @@ export CACHE_SWEEP_INTERVAL=600000 # (will be divided by SPEEDUP)
 export CACHE_ENTRY_TTL=600000 # (will be divided by SPEEDUP)
 export EDGE_ADDRESS="https://api.e2e.apigee.net"
 # temporary hack - using the same clientID for migration to access edge and for team to access permissions
-export PERMISSIONS_CLIENTID=$PERMISSIONS_MIGRATION_CLIENTID # configure this in your shell when testing
-export PERMISSIONS_CLIENTSECRET=$PERMISSIONS_MIGRATION_CLIENTSECRET # configure this in your shell when testing
+export PERMISSIONS_CLIENTID=$PERMISSIONS_CLIENTID # configure this in your shell when testing
+export PERMISSIONS_CLIENTSECRET=$PERMISSIONS_CLIENTSECRET # configure this in your shell when testing
 export CLIENT_TOKEN_ISSUER="https://login.e2e.apigee.net"
 
 source ../local-export-pg-connection-variables.sh
